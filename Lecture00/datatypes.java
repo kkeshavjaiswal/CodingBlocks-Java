5e95367474731dcf63eb5f885ecb83b66b6d821b
kkeshavjaiswal <keshav30april@gmail.com>
2024-08-28 16:14:06 +0530

    i am batman2

---
+++ Lecture00/datatypes.java
@@ -0,0 +1,24 @@
+public class datatypes {
+    public static void main(String[] args){
+        int a=10;
+        int b=12;
+        int c=-10;
+        float d=1.2f;
+        char f= '1';
+        double e=8.6d;
+        boolean g= true;
+        // System.out.println(a);
+        // System.out.println(a++);
+        // a++ gives a+1
+
+        if (a>b)
+        System.out.println(a);
+        else
+        System.out.println(b);
+
+        // && is used for and operator for conditions
+        // || is used for or operator 
+        // ^ is used for xor operator
+        // print is used for horizontal print and println is used for vertical print
+       } 
+}
---
+++ Lecture00/loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String[] args){
+       // for (initialisation; condition; updation;)
+
+       // for (int a=10; a>1; a++)
+
+       int a=1;
+       while(a<=10)
+       System.out.println(a++);
+    }
+}
